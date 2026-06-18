2312ac73c3e071d8a24759de04efe086a1f23c29
Alexandre <caratrac22@users.noreply.github.com>
2026-06-18 17:09:28 +0200

    cleanup: remove @types/youtube and 35 deployment scripts with secrets

--- deploy.py
+++
@@ -1,8 +0,0 @@
-import paramiko, sys, io
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-n = paramiko.SSHClient()
-n.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-n.connect('100.70.136.90', username='root', password='alex', timeout=30)
-s,o,e = n.exec_command("pct exec 100 -- bash -c 'cd /opt/vannes-batteries && git pull && npm run build && pm2 restart vannes-batteries'", timeout=300)
-print(o.read().decode(errors='replace'))
-n.close()
